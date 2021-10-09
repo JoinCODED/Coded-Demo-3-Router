a6d215b222d7e6ccfc8c8a1543486ba69ae54ec0
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2021-10-09 15:16:40 +0300

    ready for demo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './assets/css/animate.css';
 import './assets/css/bootstrap-5.0.0-alpha-2.min.css';
 import './assets/css/LineIcons.2.0.css';
 import './assets/css/main.css';
-import Navbar from './Components/Navbar';
+
 import Home from './Components/Home';
 import Features from './Components/Features';
 import About from './Components/About';
@@ -14,7 +14,7 @@ import featuresData from './featuresData';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+
       <Home />
       {/* code goes here */}
       {/* <Features features={featuresData} />
