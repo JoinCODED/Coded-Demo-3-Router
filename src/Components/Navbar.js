408d006334a2162a5e872c7df98e02bbbdd3ec58
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2021-10-09 15:32:16 +0300

    done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,55 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <header class="header">
-      <div class="navbar-area">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-12">
-              <nav class="navbar navbar-expand-lg">
-                <Link class="navbar-brand" to="/">
-                  Cookies Plant
-                </Link>
-                <button
-                  class="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span class="toggler-icon"></span>
-                  <span class="toggler-icon"></span>
-                  <span class="toggler-icon"></span>
-                </button>
-
-                <div
-                  class="collapse navbar-collapse sub-menu-bar"
-                  id="navbarSupportedContent"
-                >
-                  <ul id="nav" class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                      <Link class="page-scroll" to="/">
-                        Home
-                      </Link>
-                    </li>
-                    <li class="nav-item">
-                      <Link class="page-scroll" to="/features">
-                        Features
-                      </Link>
-                    </li>
-                    <li class="nav-item">
-                      <Link class="page-scroll" to="/about">
-                        About
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Link to="/">Home</Link>
+      <Link to="/features">Features</Link>
+      <Link to="/about">About</Link>
     </header>
   );
 }
